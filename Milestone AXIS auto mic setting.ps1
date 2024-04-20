46212f879d201f8756ca38021fb4b76af89e0194
Chriss Olav Furenes <49471130+ChrissFurenes@users.noreply.github.com>
2024-04-20 17:40:07 +0200

    Update Milestone AXIS auto mic setting.ps1

--- Milestone AXIS auto mic setting.ps1
+++ Milestone AXIS auto mic setting.ps1
@@ -1,15 +1,14 @@
-$server = "localhost" # endre til din server
-$path_to_config = "C:\Users\Milestone\Desktop\Milestone AXIS\" # endre til sti som inneholder konfig filer eks.."10.230.0.21.txt"
-$needs_credentials = $true # sett til $false om credentials ikke er nødvendig
-$useBasicUser = $false # sett til $true om du skal logge på milestone med basicuser
-$username = "username" #brukernavn til server pålogging
-$password = "password" #passord til server pålogging
+$server = "localhost"
+$path_to_config = "C:\Users\Milestone\Desktop\Milestone auto update axis mic konfig\"
+$needs_credentials = $true
+$useBasicUser = $false
+$username = "DESKTOP-VR3VNGT\Milestone"
+$password = "Tusse1009"
 
 $content = "application/json"
-$api = "axis-cgi/audiodevicecontrol.cgi" # API url
+$api = "axis-cgi/audiodevicecontrol.cgi"
 
-# Kobler til management server
-if($needs_credentials){ 
+if($needs_credentials){
     $server_credentials = ([PSCredential]::new($username, (ConvertTo-SecureString -String $password -AsPlainText -Force)))
     if($useBasicUser){
         Connect-ManagementServer -Server $server -Credential $server_credentials -BasicUser
@@ -21,21 +20,21 @@ if($needs_credentials){
 }
 
 $response = $null
-ForEach ($camera in Get-Hardware){ # henter alle kamera i milestone
-    if ($camera.Name.Contains("AXIS") -and $camera.Enabled){ # kjører kun når det er et AXIS enhet
-        $pass = $camera | Get-HardwarePassword #henter passord på enhet fra milestone
-        $user = $camera.UserName #henter brukernavn på enhet fra milestone
-        $ip = ($camera.Address.Substring(7) -replace “.$”) # setter verdien fra "http://10.230.0.21/" til "10.230.0.21"
-        $credentials = ([PSCredential]::new($user, (ConvertTo-SecureString -String $pass -AsPlainText -Force))) # lager credentila til å bruke til API kalling
+ForEach ($camera in Get-Hardware){
+    if ($camera.Name.Contains("AXIS") -and $camera.Enabled){
+        $pass = $camera | Get-HardwarePassword
+        $user = $camera.UserName
+        $ip = ($camera.Address.Substring(7) -replace “.$”)
+        $credentials = ([PSCredential]::new($user, (ConvertTo-SecureString -String $pass -AsPlainText -Force)))
         $url = ($camera.Address + $api)
-        if ((Test-Connection -Count 1 -Quiet $ip) -and (Test-Path ($path_to_config + $ip + ".txt") -PathType Leaf)){ # sjekker om enhet nås på ping og at konfig fil er til stede
+        if ((Test-Connection -Count 1 -Quiet $ip) -and (Test-Path ($path_to_config + $ip + ".txt") -PathType Leaf)){
         
             try{
             echo "Endrer"
             echo ("URL: " + $camera.Address + "axis-cgi/audiodevicecontrol.cgi")
             echo ("Content type: " + $content)
             #echo ("Body: " + (Get-Content -Path ($ip +".txt")))
-            $response = curl -Method Post $url -Credential $credentials -ContentType $content -Body (Get-Content -Path ($path_to_config + $ip +".txt")) # kjører API
+            $response = curl -Method Post $url -Credential $credentials -ContentType $content -Body (Get-Content -Path ($path_to_config + $ip +".txt"))
             echo "OK. Config er gjort"
             echo ($response.StatusCode)
 
@@ -45,8 +44,7 @@ ForEach ($camera in Get-Hardware){ # henter alle kamera i milestone
             }
         }else{
             echo ("Feilet. Sjekk kamera eller config! Kamera: " + $camera.Name + " med ip: " + $camera.Address)
-            break
         }
     }
 }
-Disconnect-ManagementServer # Kobler fra management server
+Disconnect-ManagementServer
